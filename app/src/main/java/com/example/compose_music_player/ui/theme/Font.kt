476c4c3c2d1769932026126dc506c3c26406f922
AdonayMejia <adonay.mejia021@gmail.com>
2023-05-14 15:48:02 -0600

    style: Adding Noto Serif Font

---
+++ app/src/main/java/com/example/compose_music_player/ui/theme/Font.kt
@@ -0,0 +1,13 @@
+package com.example.compose_music_player.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.compose_music_player.R
+
+val NotoSerif = FontFamily(
+    Font(R.font.notoserif_regular),
+    Font(R.font.notoserif_italic, FontWeight.Light),
+    Font(R.font.notoserif_bold, FontWeight.Bold),
+    Font(R.font.notoserif_bolditalic, FontWeight.Bold)
+)
